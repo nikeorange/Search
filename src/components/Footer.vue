736efd86d9a7bb8ecef7ebfaf5aa17b01af1f4a6
Petr <47572782+nikeorange@users.noreply.github.com>
2021-09-23 13:48:19 +0300

    Add files via upload

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container-fluid footer">
+    <div class="row">
+      <div class="coll github_link">
+        
+      </div>
+      <div class="coll">
+          <img src="../assets/photo.jpg" alt="photo" />
+          <strong>Petr Uba</strong>
+          <a href="https://github.com/nikeorange" target="_blank"> GitHub</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "Footer"
+
+};
+</script>
+
+<style lang="scss">
+</style>
